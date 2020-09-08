11731b7d6fe666614d5a25cf7eef0440209c802a
Didi <ofisser86@gmail.com>
2020-09-08 18:07:18 +0300

    End task 6.2 from 6 Stage

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,24 +1,3 @@
-import random
 # Write your code here
-word_list = ['python', 'java', 'kotlin', 'javascript']
 print("H A N G M A N")
-print()
-pc_word = random.choice(word_list)
-
-guessing_words = ['-' for i in range(len(pc_word))]
-for i in range(8):
-    print(''.join(guessing_words))
-    letter = input('Input a letter:').strip(' ')
-    if letter in pc_word:
-        for j in range(len(pc_word)):
-            if pc_word[j] == letter:
-                guessing_words[j] = letter
-        print()
-    else:
-        print("No such letter in the word")
-        print()
-
-print("""
-Thanks for playing!
-We'll see how well you did in the next stage
-""")
+print("The game will be available soon.")
