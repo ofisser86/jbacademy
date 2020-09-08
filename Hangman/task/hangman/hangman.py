132b93ff77daff02cd6bfb2fc5a03f96a6fce6ee
Didi <ofisser86@gmail.com>
2020-09-08 16:10:05 +0300

    Finish Stage 5/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -2,9 +2,23 @@ import random
 # Write your code here
 word_list = ['python', 'java', 'kotlin', 'javascript']
 print("H A N G M A N")
+print()
 pc_word = random.choice(word_list)
-word = input(f'Guess the word {pc_word[:3]}' + '-' * len(pc_word[3:]) + ': > ')
-if word == pc_word:
-    print("You survived!")
-else:
-    print("You are hanged!")
+
+guessing_words = ['-' for i in range(len(pc_word))]
+for i in range(8):
+    print(''.join(guessing_words))
+    letter = input('Input a letter:').strip(' ')
+    if letter in pc_word:
+        for j in range(len(pc_word)):
+            if pc_word[j] == letter:
+                guessing_words[j] = letter
+        print()
+    else:
+        print("No such letter in the word")
+        print()
+
+print("""
+Thanks for playing!
+We'll see how well you did in the next stage
+""")
---
+++ Problems/Memory test/main.py
@@ -0,0 +1,9 @@
+numbers = input().split()
+answers = input().split()
+
+
+# print(set(numbers) == set(answers))
+if set(numbers) == set(answers):
+    print(True)
+else:
+    print(False)
