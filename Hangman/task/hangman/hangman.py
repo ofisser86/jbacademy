e1034f40b30efc8b40b9233f4d846139a4bbf93f
Didi <ofisser86@gmail.com>
2020-09-08 11:53:14 +0300

    Finish Stage 4/8 + Daily task

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,3 +1,10 @@
+import random
 # Write your code here
+word_list = ['python', 'java', 'kotlin', 'javascript']
 print("H A N G M A N")
-print("The game will be available soon.")
+pc_word = random.choice(word_list)
+word = input(f'Guess the word {pc_word[:3]}' + '-' * len(pc_word[3:]) + ': > ')
+if word == pc_word:
+    print("You survived!")
+else:
+    print("You are hanged!")
