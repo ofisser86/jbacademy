6d53310f695a7a1d996218cb2f2f03e13a699b76
Didi <ofisser86@gmail.com>
2020-09-10 15:41:12 +0300

    End task 7 Stage

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -6,9 +6,12 @@ print()
 pc_word = random.choice(word_list)
 
 guessing_words = ['-' for i in range(len(pc_word))]
+wrong_letters = set()
 attempts = 0
+last_letter = 0
 while attempts < 8:
     if '-' not in guessing_words:
+        guessing_words[last_letter] = '-'
         print("".join(guessing_words))
         print("You guessed the word!")
         print("You survived!")
@@ -16,11 +19,24 @@ while attempts < 8:
 
     print(''.join(guessing_words))
     letter = input('Input a letter: ').strip(' ')
+    if letter in wrong_letters:
+        print("You already typed this letter")
+        print()
+        continue
+
+    if len(letter) != 1:
+        print('You should input a single letter')
+        print()
+        continue
+    elif not letter.isascii() or letter.isupper() or not letter.isalpha():
+        print('It is not an ASCII lowercase letter')
+        print()
+        continue
 
     if letter in pc_word:
         if letter in guessing_words:
-            print("No improvements")
-            attempts += 1
+            print("You already typed this letter")
+            # attempts += 1
             if attempts == 8:
                 print("You are hanged!")
                 break
@@ -31,13 +47,15 @@ while attempts < 8:
             for j in range(len(pc_word)):
                 if pc_word[j] == letter:
                     guessing_words[j] = letter
-        print()
+                    last_letter = j
+                    if '-' in guessing_words:
+                        print()
     else:
         print("No such letter in the word")
         attempts += 1
+        wrong_letters.add(letter)
         if attempts == 8:
             print("You are hanged!")
             break
         else:
             print()
-
