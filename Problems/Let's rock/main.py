1f2fd3b741daaf46c2c921ef51bb42a287418691
Didi <ofisser86@gmail.com>
2020-09-14 18:25:29 +0300

    Ends point 'Class vs instance' in  6/6

---
+++ Problems/Let's rock/main.py
@@ -0,0 +1,11 @@
+# start a RockBand here
+class RockBand:
+    genre = 'rock'
+    key_instruments = ["electric guitar", "drums"]
+    n_members = 4
+
+
+boombox = RockBand()
+print(boombox.genre)
+print(boombox.n_members)
+print(boombox.key_instruments)
---
+++ Problems/Who is who/main.py
@@ -0,0 +1,20 @@
+class Angel:
+    color = "white"
+    feature = "wings"
+    home = "Heaven"
+
+
+class Demon:
+    color = "red"
+    feature = "horns"
+    home = "Hell"
+
+
+me = Angel()
+me2 = Demon()
+print(me.color)
+print(me.feature)
+print(me.home)
+print(me2.color)
+print(me2.feature)
+print(me2.home)
