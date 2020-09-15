4a5fed56e170e76784951b1f40137511caefb830
Didi <ofisser86@gmail.com>
2020-09-15 18:39:14 +0300

    Ends of OOP part in Stage 5/6

--- Problems/The Louvre/main.py
+++ Problems/The Louvre/main.py
@@ -1,1 +1,11 @@
 class Painting:
+    museum = 'Louvre'
+
+    def __init__(self, title, artist, year):
+        self.title = title
+        self.artist = artist
+        self.year = year
+
+
+painter = Painting(input(), input(), input())
+print(f'"{painter.title}" by {painter.artist} ({painter.year}) hangs in the {painter.museum}.')
